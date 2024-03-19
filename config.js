f57a14bec99ff21054c6bc45267b230630dc950f
mela gura <144571368+HIghSere@users.noreply.github.com>
2024-03-19 20:22:17 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    token: 'YOUR_TOKEN'
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client } = require("discord.js-selfbot-v13");
+const config = require("./config");
+const client = new Client({
+    checkUpdate: false,
+});
+const prefix = "c<";
+
+client.on("messageCreate", async (message) => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if (message.author.id !== client.user.id) return;
+    if (command === "sugukesu") {
+        await message.reply("このメッセージは５秒後に削除されます。").then(async(msg) => {
+            await wait(5000);
+            msg.delete();
+        });
+        message.delete();
+        await message.channel.send(`メッセージの削除が完了しました！\ncontent: ${args.join(" ")}`);
+    }
+});
+
+async function wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+client.login(config.token);
